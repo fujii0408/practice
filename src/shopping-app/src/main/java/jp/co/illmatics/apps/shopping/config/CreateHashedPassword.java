b2516066aab3fc684945e4bfdc53e69a5d3f0680
fujiikotaro <fujiikotaro0408@gmail.com>
2022-08-17 18:37:44 +0900

    remove bin folder from gitignore

--- src/shopping-app/src/main/java/jp/co/illmatics/apps/shopping/config/CreateHashedPassword.java
+++ src/shopping-app/src/main/java/jp/co/illmatics/apps/shopping/config/CreateHashedPassword.java
@@ -1,9 +1,9 @@
-package jp.co.illmatics;
+package jp.co.illmatics.apps.shopping.config;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class CreateHashedPassword {
-	public static void createHashedPassword() {
+	public static void main(String[] args) {
 		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 		String hashedPassword = bCryptPasswordEncoder.encode("pass"); //""の中に生のパスワードを入力
 		
